f183d16ea34df458852143e135321bd4338e7b1f
Sergey Volkov <volkov.sergey.e@gmail.com>
2018-12-09 02:20:17 +0300

    Reduce d.delay

--- src/crawler_yandex_search/cli.py
+++ src/crawler_yandex_search/cli.py
@@ -26,7 +26,7 @@ def run(search_url: str) -> None:
     logger.info('=== START ===')
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
-        'DOWNLOAD_DELAY': 1,
+        'DOWNLOAD_DELAY': 0.5,
         'DEPTH_LIMIT': 1,
         'CONCURRENT_REQUESTS_PER_DOMAIN': 3,
         'ITEM_PIPELINES': {
